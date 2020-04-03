4c730fa50f9f944d10d6d0e889aa6a55245f6719
kauffret <62516212+kauffret@users.noreply.github.com>
2020-04-03 09:21:08 +0200

    pick a card in Table.vue

--- components/Table.vue
+++ components/Table.vue
@@ -3,14 +3,43 @@
     <div class="card-content">
       <div>
         <br/>
-        <img src="https://bulma.io/images/placeholders/64x64.png" alt="Placeholder image">
+        <img :src="getImgUrl(card.png)" v-bind:alt="card.number">
       </div>
       <div class="content">
         <br/>
         <strong>Pierre</strong> is Waiting
       </div>
 
-      <button class="button is-dark is-medium is-rounded">Draw a card</button>
+      <button @click="pick" class="button is-dark is-medium is-rounded">Draw a card</button>
     </div>
   </div>
 </template>
+
+<script>
+/* eslint-disable */
+import Deck from "./../deck.json";
+  export default {
+    data() {
+      return {
+        deck: Deck,
+        card: "",
+      }
+    },
+    methods: {
+      pick: function() {
+        console.log("pick a card for the table and show it");
+        var chosenNumber = Math.floor(Math.random() * this.deck.length);
+        this.card = this.deck[chosenNumber]; // pick the card in the deck
+        this.deck.splice(chosenNumber, 1); // remove the card in the deck
+        console.log(this.card);
+        //return this.card;
+      },
+      getImgUrl(imageName) {
+        console.log(imageName);
+        if (imageName !== undefined) {
+          return require("./../assets/" + imageName);
+        }
+      }
+    },
+  }
+</script>
--- pages/room.vue
+++ pages/room.vue
@@ -14,7 +14,7 @@
         <Player />
       </div>
       <div class="column is-half">
-        <Table />
+        <Table @pick="pickACard"/>
       </div>
       <div class="column is-one-quarter">
         <Player />
@@ -33,12 +33,49 @@
 </template>
 
 <script>
+  /* eslint-disable */
   import Player from '~/components/Player.vue'
   import Table from '~/components/Table.vue'
   export default {
     components: {
       Player,
       Table
+    },
+    data() {
+      return {
+        card: "",
+        round: 0,
+        persons: [
+          {
+            id: 0,
+            name: "Robert",
+            status: "Joue",
+            cards: []
+          },
+          {
+            id: 1,
+            name: "Kévin",
+            status: "En attente",
+            cards: []
+          }
+        ]
+      };
+    },
+    methods: {
+      pickACard: function() {
+        console.log('pick a card');
+        console.log(this.persons);
+        this.pick();
+        this.persons[this.round].cards.push(this.card);
+        this.persons[this.round].status = "En attente";
+        if (this.round + 1 === this.persons.length) {
+          this.round = 0;
+        } else {
+          this.round++;
+        }
+        this.persons[this.round].status = "Joue";
+        console.log(this.round);
+      }
     }
   }
 </script>
