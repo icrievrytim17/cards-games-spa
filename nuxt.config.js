d66f960ae8a49abcf451b35f47f9a371bdcad87d
kauffret <kilian.auffret@gmail.com>
2020-05-09 14:34:43 +0200

    Google analytics

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,7 +29,9 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [],
+  plugins: [
+    { src: '~plugins/ga.js', mode: 'client' }
+  ],
   /*
    ** Nuxt.js dev-modules
    */
