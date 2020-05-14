d348f77505db1c599028b2b0dcaa78a958c8e2ed
Auffret <kilian.auffret@gmail.com>
2020-05-14 18:52:46 +0200

    Translate Footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -4,19 +4,13 @@
       <p><strong>Play & Drink</strong> by kauffret and gllm fgn. ©2020</p>
     </div>
     <div class="content container has-text-centered">
+      <p v-html="$t('footer.hello')"></p>
       <p>
-        Say hello at
-        <a href="mailto:hello@playndrink.online" class="has-text-primary">
-          hello@playndrink.online
-        </a>
-        !
-      </p>
-      <p>
-        Built with
+        {{ $t("footer.built") }}
         <fa-icon :icon="faWineBottle" class="fa-lg has-text-black" />
-        and
+        {{ $t("footer.and") }}
         <fa-icon :icon="faHeart" class="fa-lg has-text-primary" />
-        in Brest
+        {{ $t("footer.city") }}
         <br />
       </p>
     </div>
